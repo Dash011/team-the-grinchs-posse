1b528a42b82e1c9658764746def32a8b4ad8771d
Jesse Lind <jesse.lind@gmail.com>
2019-03-07 16:15:45 -0700

    Add files via upload

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
+import { MEMBERS } from '../mock-members';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+  members = MEMBERS;
+  selectedMember: Member;
+  onSelect(member: Member): void {
+    this.selectedMember = member;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/mock-members.ts
@@ -0,0 +1,14 @@
+import { Member } from './member';
+
+export const MEMBERS: Member[] = [
+    { id: 11, name: 'FamilyMember11' },
+    { id: 12, name: 'FamilyMember12' },
+    { id: 13, name: 'FamilyMember13' },
+    { id: 14, name: 'FamilyMember14' },
+    { id: 15, name: 'FamilyMember15' },
+    { id: 16, name: 'FamilyMember16' },
+    { id: 17, name: 'FamilyMember17' },
+    { id: 18, name: 'FamilyMember18' },
+    { id: 19, name: 'FamilyMember19' },
+    { id: 20, name: 'FamilyMember20' }
+];
